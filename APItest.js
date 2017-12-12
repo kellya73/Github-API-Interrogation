0b66ddeb15b944ef237d520da9cc07e440d7a6d1
kellya73 <32706271+kellya73@users.noreply.github.com>
2017-12-12 13:57:50 +0000

    Add files via upload

---
+++ APItest.js
@@ -0,0 +1,35 @@
+var GitHubApi = require('github')
+var github = new GitHubApi({
+  debug: true
+})
+
+github.authenticate({
+  type: 'oauth',
+  token: 'Enter Token Here'
+})
+
+github.users.getFollowingForUser({
+  username: 'kellya73'
+}, function (err, res) {
+  if (err) throw err
+  //console.log("\n********FOLLOWING********\n")
+  //console.log(res)
+  var keys = Object.keys(res.data);
+  console.log("\n********FOLLOWING********\n")
+  for (x=0; x<res.data.length; x++){
+    console.log(res.data[x].login)
+  }
+})
+
+github.users.getFollowersForUser({
+  username: 'kellya73'
+}, function (err, res) {
+  if (err) throw err
+  //console.log("\n********FOLLOWERS********\n")
+  //console.log(res)
+  var keys = Object.keys(res.data);
+  console.log("\n********FOLLOWERS********\n")
+  for (x=0; x<res.data.length; x++){
+    console.log(res.data[x].login)
+  }
+})
